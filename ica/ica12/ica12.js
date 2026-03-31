7d7cbf70069988d8134f3a7c21e3ae7b10112623
laurenelbert <lael2633@colorado.edu>
2026-03-31 10:42:44 -0600

    ica12

---
+++ ica/ica12/ica12.js
@@ -0,0 +1,51 @@
+let newBtn = document.querySelector('#js-new-quote');
+
+let answerBtn = document.querySelector('#js-tweet').addEventListener('click', showAnswer)
+
+const answerText = document.querySelector ('#js-answer-text');
+
+let current = {
+    question: "",
+    answer: ""
+}
+
+
+newBtn.addEventListener('click', getQuote);
+
+const endpoint = 'https://trivia.cyberwisp.com/getrandomchristmasquestion'
+
+;
+
+async function getQuote() {
+   // alert ("WORKING!")
+   try {
+    const response = await fetch(endpoint);
+    if (!response.ok) {
+        throw Error(response.statusText)
+    }
+    const json = await response.json();
+    console.log(json)
+    displayQuote(json['question']);
+    current.question = json["question"];
+    current.answer = json["answer"];
+    console.log(current.answer);
+
+   }
+   catch (err) {
+    console.log(err)
+    alert('Failed to fetch new quote');
+   }
+}
+
+function displayQuote (quote){
+    const quoteText = document.querySelector
+    ('#js-quote-text');
+    quoteText.textContent = quote;
+    answerText.textContent = "";
+}
+
+function showAnswer(){
+    answerText.textContent = current.answer;
+
+}
+getQuote();
